2449de0a18d30c049ba727108fce053133a63473
nano6yt3 <nano6yt3@users.noreply.github.com>
2023-07-18 16:47:22 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Page</title>
+</head>
+<body>
+
+<?php
+// PHP code to print the welcome message
+echo "<h1>PoC website!</h1>";
+echo "<p>by nanobyt3</p>";
+?>
+
+</body>
+</html>
